82ab0a2bd211959fafc79d4dfac42cdf17796d6a
FFrederickW <frederickwensleyl@gmail.com>
2025-12-12 00:46:41 +0700

    first commit

--- FINAL PROJECT/app.js
+++
@@ -1,3 +0,0 @@
-function redirWhatsapp() {
-    window.location.href = "https://wa.me/6281362353339";
-}
--- FINAL PROJECT/logout.js
+++
@@ -1,4 +0,0 @@
-function logout() {
-    localStorage.removeItem("isLogin");
-    window.location.href = "login.html";
-}
